74437c12aa73cd01a01dab447d79cdbf4a5d7644
Ryan Greenhall <ryan.greenhall@gmail.com>
2012-05-06 16:37:14 +0100

    Re organising function order to fix compilation

--- src/gameoflife/core.clj
+++ src/gameoflife/core.clj
@@ -1,13 +1,5 @@
 (ns gameoflife.core)
 
-(defn number-of-living-neighbours
-  [grid cell]
-  (count (filter #(alive? grid %1) (neighbours grid cell))))
-
-(defn alive?
-  [grid cell]
-  (= :alive (aget (to-array-2d grid) (:x cell) (:y cell))))
-
 (defn neighbours
   [grid cell]
 
@@ -28,7 +20,17 @@
     (filter inside-grid? (map #(into {} {:x (+ (:x cell) (:x %))
                                          :y (+ (:y cell) (:y %))}) relative-positions))))
 
+(defn alive?
+  [grid cell]
+  (= :alive (aget (to-array-2d grid) (:x cell) (:y cell))))
 
+(defn number-of-living-neighbours
+  [grid cell]
+  (count (filter #(alive? grid %1) (neighbours grid cell))))
+
+(defn state
+  [grid cell]
+  (aget (to-array-2d grid) (:x cell) (:y cell)))
 
 (defn next-generation
   "Produces the next generation based on the given generation.
@@ -48,17 +50,29 @@
    [:dead :dead :dead :alive :alive]
    [:dead :dead :dead :alive :alive]])
 
-(defn do-it
+(defn changes
   [grid]
-  (loop [x 0
-         items grid]
-    (when items
-      (loop [y 0
-             cols (first items)]
-        (when cols   
-          (println x "," y)
-          (recur (inc y) (next cols))))
-      (recur (inc x) (next items)))))
+  (let [change-set {}]
+    (loop [x 0
+           items grid]
+      (when items
+        (loop [y 0
+               cols (first items)]
+          (when cols   
+            (println x "," y)
+            (let [state (state grid {:x x :y y})]
+              (when (= :alive state)
+                (if (> (number-of-living-neighbours grid {:x x :y y}) 3)
+                  (into {:x x :y y :state :dead} change-set))
+
+                (if (< (number-of-living-neighbours grid {:x x :y y}) 2)
+                  (into {:x x :y y :state :dead} change-set)))
+              (when (= :dead state)
+                (when (= 3 (number-of-living-neighbours grid {:x x :y y}))
+                  (into {:x x :y y :state :alive}))))
+            (recur (inc y) (next cols))))
+        (recur (inc x) (next items))))
+    change-set))
 
 
 
