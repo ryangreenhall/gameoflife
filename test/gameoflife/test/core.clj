043adfcbe589e7bdc3c293206dfbeae472779b7f
Ryan Greenhall <ryan.greenhall@gmail.com>
2012-05-06 15:20:16 +0100

    Ensuring that all eight neighbours can be found

--- test/gameoflife/test/core.clj
+++ test/gameoflife/test/core.clj
@@ -3,4 +3,16 @@
   (:use [clojure.test]))
 
 (deftest can-find-the-neighbours-of-a-cell-at-edge
-  (is (= [{:x 1 :y 0} {:x 0 :y 1} {:x 1 :y 1} ] (neighbours [] {:x 0 :y 0}))))
+  (is (= [{:x 1 :y 0}
+          {:x 0 :y 1}
+          {:x 1 :y 1}] (neighbours [] {:x 0 :y 0}))))
+
+(deftest can-find-the-neighbours-of-a-cell
+  (is (= [{:x 2, :y 1}
+          {:x 3, :y 2}
+          {:x 1, :y 2}
+          {:x 2, :y 3}
+          {:x 3, :y 1}
+          {:x 1, :y 1}
+          {:x 3, :y 3}
+          {:x 1, :y 3}] (neighbours [] {:x 2 :y 2}))))
